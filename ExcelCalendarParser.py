dfd9c5bc789281fbe89c39795541f282f8f63449
Frank Way <fgway33@gmail.com>
2021-10-22 14:06:17 -0400

    CalendarParse -- first commit, trying some things out

---
+++ ExcelCalendarParser.py
@@ -0,0 +1,6 @@
+# This python file will parse the calendar dump excel spreadsheet
+# and produce a text output suitable for editing.
+
+import numpy
+import openpyxl
+
---
+++ main.py
@@ -0,0 +1,5 @@
+import numpy
+import openpyxl
+
+if __name__ == "__main__"
+    export_calendar_data()
