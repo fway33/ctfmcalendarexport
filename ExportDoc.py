c2eadd829398aa87f9cfbd621e511552e2339d75
Frank Way <fgway33@gmail.com>
2021-10-27 08:05:35 -0400

    CalendarParse -- finished processing for cells, beginning work on output doc

---
+++ ExportDoc.py
@@ -0,0 +1,8 @@
+from docx import Document
+
+from DataStructs import locations, location_exceptions, degrees, full_list_calendar_entries, dinner
+
+
+def create_word_doc():
+    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+    print(full_list_calendar_entries)
